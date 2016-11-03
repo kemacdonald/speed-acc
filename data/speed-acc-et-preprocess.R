80bc29260bf328d904bc73336a19bbc12cdf6ad7
Kyle MacDonald <kyle.macdonald@stanford.edu>
2016-11-03 10:13:30 -0700

    finished data wrangling code

---
+++ data/speed-acc-et-preprocess.R
@@ -0,0 +1,37 @@
+################################################################################
+## PREPROCESSING SCRIPT FOR SPEED-ACC EXPERIMENT
+## read in data files and consolidate them
+##
+## KM and AV 
+################################################################################
+
+## PRELIMINARIES
+rm(list = ls())
+library(dplyr)
+library(readr)
+library(magrittr)
+library(stringr)
+source("helper_functions/et_helper.R")
+
+raw.data.path <- "1_raw_data/eye_tracker_data_files/"
+processed.data.path <- "3_final_merged_data/"
+
+## LOOP TO READ IN FILES
+all.data <- data.frame()
+files <- dir(raw.data.path,pattern="*.txt")
+
+for (file.name in files) {
+  ## printe file name, so if loop breaks, we know where
+  print(file.name)
+  
+  ## these are the two functions that are most meaningful
+  d <- read.smi.idf(paste(raw.data.path,file.name,sep=""))
+  d <- preprocess.data(d) 
+  
+  ## now here's where data get bound together
+  all.data <- bind_rows(all.data, d)
+}
+
+## WRITE DATA OUT TO CSV FOR EASY ACCESS
+write_csv(all.data, paste0(processed.data.path, "speed_acc_processed_adult_data.csv")) 
+
