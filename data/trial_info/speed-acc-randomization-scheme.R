2fc2b9eb06d8c2d16237cef9d3798c64cb2a4f73
Kyle MacDonald <kyle.macdonald@stanford.edu>
2016-11-04 20:40:04 -0700

    added packrat

--- data/trial_info/speed-acc-randomization-scheme.R
+++
@@ -1,7 +0,0 @@
-## speed-acc block psuedo-randomization
-## we get the blocks and shuffle to get order 1
-## then we reverse that order to get order 2
-set.seed(007)
-blocks <- c("face", "text", "text-no-audio", "bullseye")
-block_order1 <- sample(blocks)
-block_order2 <- rev(block_order1)
