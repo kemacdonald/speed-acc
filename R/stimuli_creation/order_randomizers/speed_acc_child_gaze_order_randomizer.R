0c7041a388a9d01145299266ad680e287f92b3f9
Kyle MacDonald <kyle.macdonald@stanford.edu>
2017-08-22 11:50:43 -0700

    adding new et data

--- R/stimuli_creation/order_randomizers/speed_acc_child_gaze_order_randomizer.R
+++ R/stimuli_creation/order_randomizers/speed_acc_child_gaze_order_randomizer.R
@@ -17,7 +17,7 @@ n_orders_in_exp == length(orders_in_experiment)
 set.seed(seed = 7)
 
 ## create all the orders 
-n_orders <- 1
+#n_orders <- 1
 
 # shuffle vectors
 
@@ -26,3 +26,17 @@ colnames(order_results) <- 'order'
 
 ## save carriers data frame to a file
 write_csv(order_results, "../../data/trial_info/speed_acc_child_gaze_orders.csv")
+
+### ADD more orders (8/17/17) because we had to run more than 44 kids to get the target sample
+n_orders_added <- 20
+orders_in_experiment_added <- replicate(n_orders_added / n_orders, 'order1') %>% 
+  c(., replicate(n_orders_added / n_orders, 'order2'))  %>%
+  c(., replicate(n_orders_added / n_orders, 'order3')) %>%
+  c(., replicate(n_orders_added / n_orders, 'order4'))
+
+
+# shuffle vectors
+order_results_added <- sample(orders_in_experiment_added) %>% as.data.frame()
+colnames(order_results_added) <- 'order'
+
+write_csv(order_results_added, "../../../data/0b_trial_information/speed_acc_child_gaze_orders_added.csv")
